eb3c09bcad13aafdd7e817178f4a9ce4a0f46656
peixinyi <peixy_j@163.com>
2025-01-10 15:07:28 +0800

    fix : PASSWROD -> PASSWORD

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM ubuntu:22.04
+FROM hengyun-docker.pkg.coding.net/enterprise-platform/docker-warehouse/ubuntu:22.04
 RUN apt-get update && apt-get install -y curl vim
 
 ENV MONGODB_HOST=127.0.0.1
 ENV MONGODB_PORT=27017
 ENV MONGODB_USERNAME=admin
-ENV MONGODB_PASSWROD=123456
+ENV MONGODB_PASSWORD=123456
 ENV MONGODB_SAMPLE=sample.*
+# MongoDB 4.0 以上使用 SCRAM-SHA-256
+# MongoDB 4.0 以下使用 SCRAM-SHA-1
+ENV MONGODB_AUTH_MECHANISM=SCRAM-SHA-256
+ENV MONGODB_AUTH_SOURCE=admin
+
 
 RUN openssl req -nodes -newkey rsa:2048 -keyout kayakwiseDE.key -out kayakwiseDE.crt -x509 -days 365 -subj "/C=US/ST=kayakwiseDE/L=kayakwiseDE/O=kayakwiseDE Security/OU=IT Department/CN=kayakwise.com"
 RUN cat kayakwiseDE.crt kayakwiseDE.key > kayakwiseDE.pem
@@ -23,3 +28,4 @@ RUN cat kayakwiseDE.crt kayakwiseDE.key > kayakwiseDE.pem
 
 WORKDIR /root
 CMD [ "/bin/sh","-c","/root/start.sh" ]
+
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mongosqld --addr=0.0.0.0:3307 --mongo-uri "mongodb://${MONGODB_HOST}:${MONGODB_PORT}/?socketTimeoutMS=360000&connectTimeoutMS=360000" --schemaRefreshIntervalSecs=3600 --auth -u ${MONGODB_USERNAME} -p ${MONGODB_PASSWROD} --sslMode "allowSSL" --sslPEMKeyFile "/ssl/kayakwiseDE.pem" --sslAllowInvalidCertificates --minimumTLSVersion "TLS1_0" --mongo-authenticationSource admin --sampleNamespaces "${MONGODB_SAMPLE}"
+mongosqld --addr=0.0.0.0:3307 --mongo-uri "mongodb://${MONGODB_HOST}:${MONGODB_PORT}/?socketTimeoutMS=360000&connectTimeoutMS=360000" --schemaRefreshIntervalSecs=3600 --auth -u "${MONGODB_USERNAME}" -p "${MONGODB_PASSWORD}" --sslMode "allowSSL" --sslPEMKeyFile "/ssl/kayakwiseDE.pem" --sslAllowInvalidCertificates --minimumTLSVersion "TLS1_0" --mongo-authenticationMechanism "${MONGODB_AUTH_MECHANISM}" --mongo-authenticationSource "${MONGODB_AUTH_SOURCE}" --sampleNamespaces ''${MONGODB_SAMPLE}''
+
